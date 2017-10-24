96aef5698287692fe58931e768f2827c6ccd5e97
Devin Miller <edwin0258@gmail.com>
2017-10-24 17:30:16 +0000

    Delete exercises folder

--- exercises/haskell/leap/src/LeapYear.hs
+++
@@ -1,9 +0,0 @@
-module LeapYear (isLeapYear) where
-
-isLeapYear :: Integer -> Bool
-isLeapYear year 
-    | modYear 400 = True
-    | modYear 100 = False
-    | modYear 4 = True
-    | otherwise = False
-    where modYear i = year `mod` i == 0
